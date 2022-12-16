295f4397479a6c9aeff441c111cb7ed33ad0d6e8
Artem <mart_22@ukr.net>
2022-12-16 18:27:25 +0200

    Edit top section content in blocksStyles.js

--- src/js/modules/blocksStyles.js
+++ src/js/modules/blocksStyles.js
@@ -1,4 +1,28 @@
 export default function blockStyles() {
+	if (document.querySelector('#top-section')) {
+		document.body.classList.add('index-page');
+	} else {
+		document.body.classList.add('inner-page');
+	}
+
+	if (
+		document.querySelector('header .logo-company strong') &&
+		document.querySelector('#top-content')
+	) {
+		const title = document.querySelector('header .logo-company strong'),
+			content = document.querySelector('#top-content');
+
+		if (window.matchMedia('(min-width: 1201px)').matches) {
+			content.style.marginLeft = `${title.offsetLeft + 16}px`;
+			content.style.opacity = 1;
+		}
+
+		if (window.matchMedia('(max-width: 1200px)').matches) {
+			content.style.marginLeft = '';
+			content.style.opacity = '';
+		}
+	}
+
 	if (document.querySelector('.map-wrapper')) {
 		const observer = new IntersectionObserver(
 			(entries, observer) => {
