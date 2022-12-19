83ed9ad181d9653ed0108caa1b894502a896b741
Artem <mart_22@ukr.net>
2022-12-20 00:56:22 +0200

    Edit blockStyles.js

--- src/js/modules/blocksStyles.js
+++ src/js/modules/blocksStyles.js
@@ -1,5 +1,5 @@
 export default function blockStyles() {
-	if(document.querySelector('header')){
+	if (document.querySelector('header')) {
 		document.querySelector('header').classList.add('init');
 	}
 
@@ -9,51 +9,6 @@ export default function blockStyles() {
 		document.body.classList.add('inner-page');
 	}
 
-	if (
-		document.querySelector('header .header__logo-company strong') &&
-		document.querySelector('#top-content')
-	) {
-		const title = document.querySelector('header .header__logo-company'),
-			content = document.querySelector('#top-content');
-
-		if (window.matchMedia('(min-width: 1201px)').matches) {
-			content.style.marginLeft = `${title.offsetWidth}px`;
-			content.style.opacity = 1;
-		}
-
-		if (window.matchMedia('(max-width: 1200px)').matches) {
-			content.style.marginLeft = '';
-			content.style.opacity = '';
-		}
-	}
-
-	if (
-		document.querySelector('header .header__logo-company') &&
-		document.querySelector('.breadcrumbs') &&
-		document.querySelector('.breadcrumbs + h1') &&
-		!document.querySelector('.top-content') &&
-		!document.querySelector('.product-details__content')
-	) {
-		const title = document.querySelector('header .header__logo-company'),
-			list = document.querySelector('.breadcrumbs'),
-			mainTitle = document.querySelector('.breadcrumbs + h1');
-
-		if (window.matchMedia('(min-width: 1201px)').matches) {
-			list.style.marginLeft = `${title.offsetWidth}px`;
-			list.style.opacity = 1;
-
-			mainTitle.style.marginLeft = `${title.offsetWidth}px`;
-			mainTitle.style.opacity = 1;
-		}
-
-		if (window.matchMedia('(max-width: 1200px)').matches) {
-			list.style.marginLeft = '';
-			list.style.opacity = '';
-
-			mainTitle.style.marginLeft = '';
-			mainTitle.style.opacity = '';
-		}
-	}
 	if (document.querySelector('#order-form__quantity')) {
 		document.querySelector('#order-form__quantity').addEventListener('input', function (e) {
 			e.target.value = e.target.value.replace(/\D/g, '').substr(0, 10);
